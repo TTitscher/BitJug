602735e6f3b0c67f1c108970cf69bbc6ca8a41ad
Thomas Titscher <thomas.titscher@gmail.com>
2020-10-23 10:26:24 +0200

    Going 3D

--- adaptation/how_about_fenics_helm.py
+++ adaptation/how_about_fenics_helm.py
@@ -10,7 +10,35 @@ def simp(x):
     p = Constant(3.)
     _eps = Constant(1.e-6)
     return _eps + (1 - _eps) * x ** p
-        
+       
+
+def build_nullspace(V, x):
+    """Function to build null space for 3D elasticity"""
+
+    # Create list of vectors for null space
+    nullspace_basis = [x.copy() for i in range(6)]
+
+    # Build translational null space basis
+    V.sub(0).dofmap().set(nullspace_basis[0], 1.0);
+    V.sub(1).dofmap().set(nullspace_basis[1], 1.0);
+    V.sub(2).dofmap().set(nullspace_basis[2], 1.0);
+
+    # Build rotational null space basis
+    V.sub(0).set_x(nullspace_basis[3], -1.0, 1);
+    V.sub(1).set_x(nullspace_basis[3],  1.0, 0);
+    V.sub(0).set_x(nullspace_basis[4],  1.0, 2);
+    V.sub(2).set_x(nullspace_basis[4], -1.0, 0);
+    V.sub(2).set_x(nullspace_basis[5],  1.0, 1);
+    V.sub(1).set_x(nullspace_basis[5], -1.0, 2);
+
+    for x in nullspace_basis:
+        x.apply("insert")
+
+    # Create vector space basis and orthogonalize
+    basis = VectorSpaceBasis(nullspace_basis)
+    basis.orthonormalize()
+
+    return basis 
     
 class MeshFilter:
     def __init__(self, mesh, **kwargs):
@@ -87,7 +115,7 @@ class FEM:
         self.dxf = Function(filt.Vxf)
 
         self.bcs = []
-        self.bcs.append(DirichletBC(self.Vu, (0.,0.), plane_at(0., "x")))
+        self.bcs.append(DirichletBC(self.Vu, (0.,0.,0.), plane_at(0., "x")))
        
         
         def eps(u):
@@ -110,24 +138,43 @@ class FEM:
         self.t = 0.
 
 
+        self.f = dot(Constant((0., 0., 0.)) , u_) * dx 
         self.F = Function(self.Vu).vector()
-        P = PointSource(self.Vu.sub(1), Point(*load_at), 10)
-        P.apply(self.F)
+        self.P = PointSource(self.Vu.sub(1), Point(*load_at), 10)
         
         
         self.dKdxU = derivative(self.a * self.u, filt.xf)
         self.dc_dxf_form = -derivative((self.a * self.u) * self.u, filt.xf)
 
         self.K = PETScMatrix()
-        self.solver = LUSolver(self.K, "mumps")
+        self.null_space = build_nullspace(self.Vu, self.u.vector())
 
+        pc = PETScPreconditioner("petsc_amg")
+
+        # Use Chebyshev smoothing for multigrid
+        PETScOptions.set("mg_levels_ksp_type", "chebyshev")
+        PETScOptions.set("mg_levels_pc_type", "jacobi")
+
+        # Improve estimate of eigenvalues for Chebyshev smoothing
+        PETScOptions.set("mg_levels_esteig_ksp_type", "gmres")
+        PETScOptions.set("mg_levels_ksp_chebyshev_esteig_steps", 50)
+        
+        self.solver = PETScKrylovSolver("gmres", pc)
+        self.solver.set_operator(self.K)
+        # self.solver.parameters["maximum_iterations"] = 100
+        # self.solver.parameters["relative_tolerance"] = 1.e-6
+        
+
+    @profile
     def goal(self, x, dobj=None):
         self.filt.filter_density(x)
-       
-        assemble(self.a, tensor=self.K)
-        for bc in self.bcs:
-            bc.apply(self.K)
-
+     
+        assemble_system(self.a, self.f, self.bcs, A_tensor=self.K, b_tensor=self.F)
+        self.K.set_near_nullspace(self.null_space)
+        
+        self.P.apply(self.F)
+        
+        # solver = LUSolver(self.K, "mumps")
         self.solver.solve(self.u.vector(), self.F)
 
         J = self.F.inner(self.u.vector())
@@ -144,19 +191,31 @@ class FEM:
         return J
         
 if __name__ == "__main__":
-    set_log_level(50)
+    # set_log_level(50)
    
-    lx, ly = 180, 60
+    lx, ly, lz = 60, 30, 20
     # lx, ly = 360, 120
-    nx, ny = lx, ly
-    load_at = (lx, ly/2)
-    mesh = RectangleMesh(Point(0,0), Point(lx, ly), nx, ny)        
+    nx, ny, nz = lx, ly, lz
+    # load_at = (lx, ly/2)
 
-    filt = HelmholtzFilter(mesh, R=1)
-    filt = MeshFilter(mesh)
+
+
+    from mshr import Box, generate_mesh
+
+
+    # mesh = RectangleMesh(Point(0,0), Point(lx, ly), nx, ny)
+    # mesh = BoxMesh(Point(0.,0.,0.), Point(lx, ly, lz), nx, ny, 4)
+    geometry = Box(Point(0,0,0), Point(lx, ly, lz))
+    mesh = generate_mesh(geometry, 120)
+
+
+    load_at = (lx, ly/2., lz/2.)
+
+    filt = HelmholtzFilter(mesh, R=0.5)
+    # filt = MeshFilter(mesh)
     fem = FEM(mesh, load_at, filt)
 
-    volfrac = 0.3
+    volfrac = 0.1
     volume_constraint = lambda x, diff: filt.average_density(x, diff) - volfrac
 
     x0 = filt.x.vector()[:]
